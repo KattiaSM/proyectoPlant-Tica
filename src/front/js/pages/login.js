316f4ec0f4774894b6e9523cb4b284dff0c3ef8d
byronvillalobos <byronvillalobos@hotmail.com>
2021-04-20 19:47:30 +0000

    agregando modelo DB

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -3,10 +3,68 @@ import PropTypes from "prop-types";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
-export function Login() {
+export const Login = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		const body = {
+			email: email,
+			password: password
+		};
+
+		fetch("https://3000-lime-mollusk-0goukxq3.ws-us03.gitpod.io/login", {
+			method: "POST",
+			body: JSON.stringify(body),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				sessionStorage.setItem("my_token", data.token);
+			})
+			.catch(err => console.log(err));
+	};
+
 	return (
-		<div className="m-5">
-			<h1>HOLA! TRABAJÁ ACÁ EL LOGIN</h1>
+		// <div className="mx-auto pt-5">
+        <div className="m-5">
+			<h1>Login</h1>
+			<form onSubmit={handleSubmit} style={{ width: "500px" }}>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail1" className="form-label">
+						Email address
+					</label>
+					<input
+						onChange={e => setEmail(e.target.value)}
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+					/>
+					<div id="emailHelp" className="form-text">
+						Well never share your email with anyone else.
+					</div>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleInputPassword1" className="form-label">
+						Password
+					</label>
+					<input
+						onChange={e => setPassword(e.target.value)}
+						type="password"
+						className="form-control"
+						id="exampleInputPassword1"
+					/>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
 		</div>
 	);
-}
+};
