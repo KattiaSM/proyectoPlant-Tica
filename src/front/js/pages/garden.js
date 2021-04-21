d33bdca3b2311a1743ccac05baa9710082ba6680
Kattia Salazar Mesen <kattiasm@gmail.com>
2021-04-21 22:27:05 +0000

    posible garden_parte2

--- src/front/js/pages/garden.js
+++ src/front/js/pages/garden.js
@@ -5,8 +5,52 @@ import { Link } from "react-router-dom";
 
 export function Garden() {
 	return (
-		<div className="m-5">
-			<h1>HOLA! TRABAJÁ ACÁ EL GARDEN</h1>
+		<div className="container">
+			<div className="row">
+				<div className="col-sm-8">
+					<img src="https://via.placeholder.com/550x350/" className="img-responsive" alt="" />
+				</div>
+				<div className="col-sm-4">
+					<div className="box">
+						<h2>My Plants</h2>
+						<h3>To Do List</h3>
+					</div>
+					<div className="box">
+						<ul className="list-unstyled">
+							<li>
+								<input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+								<label className="form-check-label" htmlFor="flexCheckDefault">
+									Agua
+								</label>
+							</li>
+							<li>
+								<input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+								<label className="form-check-label" htmlFor="flexCheckDefault">
+									Abono
+								</label>
+							</li>
+							<li>
+								<input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+								<label className="form-check-label" htmlFor="flexCheckDefault">
+									Luz
+								</label>
+							</li>
+							<li>
+								<input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+								<label className="form-check-label" htmlFor="flexCheckDefault">
+									Poda
+								</label>
+							</li>
+							<li>
+								<input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+								<label className="form-check-label" htmlFor="flexCheckDefault">
+									Sustrato
+								</label>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
