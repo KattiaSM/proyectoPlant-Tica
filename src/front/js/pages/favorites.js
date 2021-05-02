fe43f6f4218221c3895bb79b4dfba65aa6229617
fabianchs <fabichasola@hotmail.com>
2021-05-02 21:03:49 +0000

    favoritos boton borrar

--- src/front/js/pages/favorites.js
+++ src/front/js/pages/favorites.js
@@ -5,56 +5,60 @@ import { Link } from "react-router-dom";
 import { Card, CardBody, Button, CardTitle, CardText, CardImg } from "reactstrap";
 
 export function Favorites() {
-	let data = [
-		{
-			scientific_name: "Quercus Rotundifolia",
-			synonymous: "Roble de Hoja Perenne",
-			year: "1785",
-			family: "Fagaceae",
-			scientific_expanded: "Quercus rotundifolia f. dolichocalyx",
-			url: "https://bs.plantnet.org/image/o/1a03948baf0300da25558c2448f086d39b41ca30"
-		},
-		{
-			scientific_name: "Urtica-diotica",
-			synonymous: "Ortiga",
-			year: "1753",
-			family: "Urticaceae",
-			scientific_expanded: "Urtica haussknechtii",
-			url: "https://bs.plantnet.org/image/o/85256a1c2c098e254fefe05040626a4df49ce248"
-		},
-		{
-			scientific_name: "Dactylis glomerata",
-			synonymous: "Pastos de la huerta",
-			year: "1753",
-			family: "Poaceae",
-			scientific_expanded: "Dactylis glomerata subsp. himalayensis",
-			url: "https://bs.plantnet.org/image/o/428f40dadfa0281dc890ead17fcd07882f9efb09"
-		},
-		{
-			scientific_name: "Festuca rubra",
-			synonymous: "Festuca roja",
-			year: "1753",
-			family: "Fagaceae",
-			scientific_expanded: "Quercus rotundifolia f. dolichocalyx",
-			url: "https://bs.plantnet.org/image/o/0b932c8a275efc79f473a71bec20d6f15e9b6b90"
-		},
-		{
-			scientific_name: "Plantago lanceolata",
-			synonymous: "Plátano de hoja estrecha",
-			year: "1753",
-			family: "Poaceae",
-			scientific_expanded: "Festuca austrodolomitica",
-			url: "https://bs.plantnet.org/image/o/78a8374f009e6ed2dc71ca17d18e4271ea0a2a7b"
-		},
-		{
-			scientific_name: "Quercus robur",
-			synonymous: "Roble inglés",
-			year: "1753",
-			family: "Fagaceae",
-			scientific_expanded: "Quercus robur subsp. broteroan",
-			url: "https://bs.plantnet.org/image/o/2292b670683abdaac354389514105df0018d9ef8"
-		}
-	];
+	const [data, setData] = useState([]); //Almacena las plantas
+
+	useEffect(() => {
+		setData([
+			{
+				scientific_name: "Quercus Rotundifolia",
+				synonymous: "Roble de Hoja Perenne",
+				year: "1785",
+				family: "Fagaceae",
+				scientific_expanded: "Quercus rotundifolia f. dolichocalyx",
+				url: "https://bs.plantnet.org/image/o/1a03948baf0300da25558c2448f086d39b41ca30"
+			},
+			{
+				scientific_name: "Urtica-diotica",
+				synonymous: "Ortiga",
+				year: "1753",
+				family: "Urticaceae",
+				scientific_expanded: "Urtica haussknechtii",
+				url: "https://bs.plantnet.org/image/o/85256a1c2c098e254fefe05040626a4df49ce248"
+			},
+			{
+				scientific_name: "Dactylis glomerata",
+				synonymous: "Pastos de la huerta",
+				year: "1753",
+				family: "Poaceae",
+				scientific_expanded: "Dactylis glomerata subsp. himalayensis",
+				url: "https://bs.plantnet.org/image/o/428f40dadfa0281dc890ead17fcd07882f9efb09"
+			},
+			{
+				scientific_name: "Festuca rubra",
+				synonymous: "Festuca roja",
+				year: "1753",
+				family: "Fagaceae",
+				scientific_expanded: "Quercus rotundifolia f. dolichocalyx",
+				url: "https://bs.plantnet.org/image/o/0b932c8a275efc79f473a71bec20d6f15e9b6b90"
+			},
+			{
+				scientific_name: "Plantago lanceolata",
+				synonymous: "Plátano de hoja estrecha",
+				year: "1753",
+				family: "Poaceae",
+				scientific_expanded: "Festuca austrodolomitica",
+				url: "https://bs.plantnet.org/image/o/78a8374f009e6ed2dc71ca17d18e4271ea0a2a7b"
+			},
+			{
+				scientific_name: "Quercus robur",
+				synonymous: "Roble inglés",
+				year: "1753",
+				family: "Fagaceae",
+				scientific_expanded: "Quercus robur subsp. broteroan",
+				url: "https://bs.plantnet.org/image/o/2292b670683abdaac354389514105df0018d9ef8"
+			}
+		]);
+	}, []);
 
 	//array.map(function(currentValue, index, arr), thisValue)
 
@@ -82,6 +86,19 @@ export function Favorites() {
 		counter++;
 	});
 
+	//La siguiente función se encarga de eliminar un favorito, recibe un nombre y lo compara, si lo encuentra, elimina el favorito
+
+	function deleteFav(name) {
+		let temporal = data;
+		temporal.map((item, index) => {
+			if (name === item.scientific_name) {
+				temporal.splice(index, 1);
+				console.log("Encontrado name");
+			}
+		});
+		setData(temporal);
+	}
+
 	//La siguiente función lo que hace es darle formato html a cada elemento de los tres array
 	//Esta función es llamada por otra variable abajo, que almacena el return de esta función
 	function format(array) {
@@ -89,7 +106,7 @@ export function Favorites() {
 			<Card key={index}>
 				<CardImg top width="50%" src={item.url} alt="Card image cap" />
 				<div className="row d-flex justify-content-end">
-					<Button className="col-2" close />
+					<Button className="col-2" onClick={() => deleteFav(item.scientific_name)} close />
 				</div>
 				<CardBody>
 					<CardTitle tag="h5">{item.scientific_name}</CardTitle>
@@ -109,6 +126,12 @@ export function Favorites() {
 	let col_1 = format(cards_col1);
 	let col_2 = format(cards_col2);
 	let col_3 = format(cards_col3);
+
+	// useEffect(() => {
+	// 	let col_1 = format(cards_col1);
+	// 	let col_2 = format(cards_col2);
+	// 	let col_3 = format(cards_col3);
+	// });
 
 	return (
 		<div className="m-5">
